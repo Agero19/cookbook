83a3cc599e0679345d2787bfc2ea159e81752d43
Agero19 <pmotulko@gmail.com>
2024-11-21 14:02:21 +0100

    Add Footer

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <div className="container">
           {/* Logo image */}
           <a
-            href="#"
+            href="index"
             className="main-logo"
           >
             <img
@@ -22,19 +22,19 @@ function App() {
           {/* Navbar values  */}
           <ul className="main-nav">
             <li className="nav-item">
-              <a href="#">Home</a>
+              <a href="/">Home</a>
             </li>
             <li className="nav-item">
-              <a href="#">Recipes</a>
+              <a href="/">Recipes</a>
             </li>
             <li className="nav-item">
-              <a href="#">Blog</a>
+              <a href="/">Blog</a>
             </li>
             <li className="nav-item">
-              <a href="#">Contact</a>
+              <a href="/">Contact</a>
             </li>
             <li className="nav-item">
-              <a href="#">About us</a>
+              <a href="/">About us</a>
             </li>
           </ul>
 
@@ -68,10 +68,76 @@ function App() {
       </header>
       <footer className="main-footer">
         <div className="container">
-          <div className="footer-top"></div>
+          {/* Top part of footer */}
+
+          <div className="footer-top">
+            {/* Slogan with logo */}
+
+            <div className="info">
+              <a
+                href="index"
+                className="main-logo"
+              >
+                <img
+                  src={logo}
+                  alt="logo"
+                />
+              </a>
+
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            {/* Page footer Navigation  */}
+
+            <ul className="main-nav">
+              <li className="nav-item">
+                <a href="/">Recipes</a>
+              </li>
+              <li className="nav-item">
+                <a href="/">Blog</a>
+              </li>
+              <li className="nav-item">
+                <a href="/">Contact</a>
+              </li>
+              <li className="nav-item">
+                <a href="/">About us</a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Lower part of footer */}
           <div className="footer-copyright">
-            <div className="copyright"></div>
-            <div className="social-links"></div>
+            <div className="copyright">
+              <p>
+                © 2020 Flowbase. Powered by
+                <span className="highlighted"> Webflow</span>
+              </p>
+            </div>
+
+            <div className="social-wrap">
+              <div className="social-links">
+                <button className="social-links-button">
+                  <img
+                    src={insta}
+                    className="social-logo"
+                    alt="logo"
+                  />
+                </button>
+                <button className="social-links-button">
+                  <img
+                    src={insta}
+                    className="social-logo"
+                    alt="logo"
+                  />
+                </button>
+                <button className="social-links-button">
+                  <img
+                    src={insta}
+                    className="social-logo"
+                    alt="logo"
+                  />
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </footer>
